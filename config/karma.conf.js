c78371dcd681d0fa57bfd614109eff8e6544af9d
KFlash <YOUR EMAIL ADDRESS>
2016-06-07 21:39:42 +0800

    more issues fixed

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -31,12 +31,30 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: ['PhantomJS'],
-        singleRun: true,
+        browsers: ['Chrome'],
+	    customLaunchers: {
+		    Chrome_travis_ci: {
+			    base: 'Chrome',
+			    flags: ['--no-sandbox']
+		    }
+	    },
+	    singleRun: true,
         plugins: ['karma-*'],
         client: {
             mocha: { ui: 'bdd' }
         }
     };
+
+	if (process.env.APPVEYOR) {
+		configuration.browsers = ['IE'];
+		configuration.singleRun = true;
+		configuration.browserNoActivityTimeout = 90000; // Note: default value (10000) is not enough
+	}
+
+	if (process.env.TRAVIS || process.env.CIRCLECI) {
+		configuration.browsers = ['Chrome_travis_ci'];
+		configuration.singleRun = true;
+	}
+
     config.set(configuration);
 };
