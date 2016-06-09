863faa4fbb46e1fbba145f54eb7e8cb28845748d
KFlash <YOUR EMAIL ADDRESS>
2016-06-09 15:00:45 +0800

    fixed travis.yml fixes

--- test/node-tests/trixly.js
+++ test/node-tests/trixly.js
@@ -1,12 +1,7 @@
 var chai_1 = require('chai');
-var chai_2 = require('chai');
-var sinon = require('sinon');
 describe('Trixly - server tests', function () {
     it('works', function () {
         chai_1.expect(true).to.be.true;
-    });
-    it('sinon is available', function () {
-        chai_2.assert.ok(sinon.spy);
     });
 });
 //# sourceMappingURL=trixly.js.map
--- test/node-tests/trixly.ts
+++ test/node-tests/trixly.ts
@@ -1,16 +1,9 @@
 import { expect } from 'chai';
-import {assert} from 'chai';
-import * as sinon from 'sinon';
 
 describe('Trixly - server tests', () => {
 
     it('works', () => {
         expect(true).to.be.true;
     });
-
-    it('sinon is available', () => {
-        assert.ok(sinon.spy);
-    });
-
 });
 
